5ca6d16f1218ba6d0047dc17677b263eb2fcac7b
Hal Deadman <hal.deadman@gmail.com>
2020-04-21 22:40:59 -0400

    update .gitattributes b/c vmoptions can't have crlf

--- build_aws.sh
+++ build_aws.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-mvn -DskipDockerPublish=false -DdockerImageTag=057602032570.dkr.ecr.us-east-1.amazonaws.com/pwm/pwm-webapp:latest package
-
+mvn -DskipDockerPublish=false -DdockerImageTag=057602032570.dkr.ecr.us-east-1.amazonaws.com/pwm/pwm-webapp:latest clean package
+docker load < docker/target/pwm-docker-image*.tar
+docker rm pwm 
+docker run -d --name pwm 057602032570.dkr.ecr.us-east-1.amazonaws.com/pwm/pwm-webapp:latest
+docker logs pwm
